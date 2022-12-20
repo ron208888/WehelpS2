7344493fa323b5810e008f490e018709f3cbe3cd
ron <ron208888@gmail.com>
2022-12-21 05:02:10 +0800

    W6 first done

--- order.py
+++ order.py
@@ -16,7 +16,7 @@ dbconfig = {
     "host": "localhost",
     "database": "taipei_day_trip",
     "user": "root",
-    "passwd": "12345678",
+    "passwd": "",
     "charset": "utf8mb4", "auth_plugin": 'mysql_native_password'}
 
 cnxpool = mysql.connector.pooling.MySQLConnectionPool(
